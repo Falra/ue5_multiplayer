ea2a3bfb3a75604d1a6022e8475c56623793683c
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-07-04 11:55:43 +0300

    #039: Add blank anim instance class

---
+++ Source/Blaster/Character/BlasterAnimInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BlasterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API UBlasterAnimInstance : public UAnimInstance
+{
+    GENERATED_BODY()
+};
