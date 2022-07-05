344a803e452d74a8067053aa8025b7afd4ee9e3c
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-07-05 19:52:43 +0300

    #040: Setup seamless travel

---
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+    Super::PostLogin(NewPlayer);
+
+    const auto NumberOfPlayers = GameState.Get()->PlayerArray.Num();
+    if(NumberOfPlayers == 2)
+    {
+        if(const auto World = GetWorld())
+        {
+            bUseSeamlessTravel = true;
+            World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+        }
+    }
+}
