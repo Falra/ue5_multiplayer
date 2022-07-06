b4eaebaf8e9c2007f24301b5837518c613469dad
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-07-06 19:58:45 +0300

    #041: Add blank UMG class for overhead

---
+++ Source/Blaster/HUD/OverheadWidget.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OverheadWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API UOverheadWidget : public UUserWidget
+{
+    GENERATED_BODY()
+};
