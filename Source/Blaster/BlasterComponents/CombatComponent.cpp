ccb27abe05b0e720d54bf299fc80c6f473af84e7
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-07-08 18:14:16 +0300

    #046: Add empty combat component class

---
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -0,0 +1,15 @@
+// Blaster Game. All Rights Reserved
+
+
+#include "CombatComponent.h"
+
+UCombatComponent::UCombatComponent()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UCombatComponent::BeginPlay()
+{
+    Super::BeginPlay();
+}
---
+++ Source/Blaster/BlasterComponents/CombatComponent.h
@@ -0,0 +1,21 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CombatComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class BLASTER_API UCombatComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UCombatComponent();
+
+protected:
+    virtual void BeginPlay() override;
+
+};
