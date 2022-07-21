c0707a91797891cc1f978417b17c0ec0f97fbc52
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-07-21 13:03:30 +0300

    #081: Add blank player controller class

---
+++ Source/Blaster/PlayerController/BlasterPlayerController.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BlasterPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ABlasterPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+};
