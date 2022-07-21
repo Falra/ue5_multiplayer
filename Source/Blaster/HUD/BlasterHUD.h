c0aa9b3635f63d63c231379ae9c6073177695438
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-07-21 13:05:19 +0300

    #081: Add blank HUD class

---
+++ Source/Blaster/HUD/BlasterHUD.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "BlasterHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ABlasterHUD : public AHUD
+{
+    GENERATED_BODY()
+};
