ef2bd0e328cfca0fc4404c4c80faf19b4ed37b9f
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-07-26 08:12:23 +0300

    #098: Add blank bullet class

---
+++ Source/Blaster/Weapon/ProjectileBullet.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile.h"
+#include "ProjectileBullet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AProjectileBullet : public AProjectile
+{
+    GENERATED_BODY()
+};
