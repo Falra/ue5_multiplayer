28cb7ac395b15f813a45c41a0344c29f6552bdce
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-06 16:21:40 +0300

    #131: Add blank game state class

---
+++ Source/Blaster/GameState/BlasterGameState.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "BlasterGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ABlasterGameState : public AGameState
+{
+    GENERATED_BODY()
+};
