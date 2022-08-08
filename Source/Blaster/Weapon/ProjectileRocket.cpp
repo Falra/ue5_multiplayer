5ac0082e3022521401ceab53d73aa06145802707
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-08 08:10:15 +0300

    #133: Implement rocket projectile logic

--- Source/Blaster/Weapon/ProjectileRocket.cpp
+++ Source/Blaster/Weapon/ProjectileRocket.cpp
@@ -2,3 +2,19 @@
 
 
 #include "ProjectileRocket.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void AProjectileRocket::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+    const FHitResult& Hit)
+{
+    if (const APawn* FiringPawn = GetInstigator())
+    {
+        if (AController* FiringController = FiringPawn->GetController())
+        {
+            UGameplayStatics::ApplyRadialDamageWithFalloff(this, Damage, RocketMinimumDamage, GetActorLocation(), RocketDamageInnerRadius,
+                RocketDamageOuterRadius, 1.0f, UDamageType::StaticClass(), TArray<AActor*>(), this, FiringController);
+        }
+    }
+    Super::OnHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit);
+}
--- Source/Blaster/Weapon/ProjectileRocket.h
+++ Source/Blaster/Weapon/ProjectileRocket.h
@@ -13,4 +13,15 @@ UCLASS()
 class BLASTER_API AProjectileRocket : public AProjectile
 {
     GENERATED_BODY()
+
+protected:
+    virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
+
+private:
+    UPROPERTY(EditDefaultsOnly, Category = "Rocket")
+    float RocketMinimumDamage = 10.0f;
+    UPROPERTY(EditDefaultsOnly, Category = "Rocket")
+    float RocketDamageInnerRadius = 200.0f;
+    UPROPERTY(EditDefaultsOnly, Category = "Rocket")
+    float RocketDamageOuterRadius = 500.0f;
 };
