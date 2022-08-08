529cd9f9bda53196bad91558088de821d282b2b3
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-08 07:53:54 +0300

    #133: Add blank class for rocket projectile

---
+++ Source/Blaster/Weapon/ProjectileRocket.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile.h"
+#include "ProjectileRocket.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AProjectileRocket : public AProjectile
+{
+    GENERATED_BODY()
+};
