e5340c8ddcdd420268403e89ec7b0a0541454117
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-09 08:32:58 +0300

    #136: Add blank class for rocket movement component

---
+++ Source/Blaster/Weapon/RocketMovementComponent.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "RocketMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API URocketMovementComponent : public UProjectileMovementComponent
+{
+    GENERATED_BODY()
+};
