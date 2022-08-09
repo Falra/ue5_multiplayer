81bafbfe786a984c7148ad81f596b7bd4b0a295a
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-09 09:03:11 +0300

    #137: Add blank class for hit scan weapon

---
+++ Source/Blaster/Weapon/HitScanWeapon.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "HitScanWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AHitScanWeapon : public AWeapon
+{
+    GENERATED_BODY()
+};
