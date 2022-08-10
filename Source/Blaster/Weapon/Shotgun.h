3ac133cedbfb17d1b95bdd066bd8344fccdeafb4
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-10 08:47:35 +0300

    #141: Add blank class for Shotgun

---
+++ Source/Blaster/Weapon/Shotgun.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HitScanWeapon.h"
+#include "Shotgun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AShotgun : public AHitScanWeapon
+{
+    GENERATED_BODY()
+};
