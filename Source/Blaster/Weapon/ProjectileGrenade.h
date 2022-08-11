81fdf8dc52aafb5f668295dba7154ba7430bd5cb
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-11 20:25:05 +0300

    #146: Add blank class for grenade projectile

---
+++ Source/Blaster/Weapon/ProjectileGrenade.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile.h"
+#include "ProjectileGrenade.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AProjectileGrenade : public AProjectile
+{
+    GENERATED_BODY()
+};
