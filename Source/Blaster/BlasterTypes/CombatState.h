9c3564fa536f84585f16f1f74efd15304106dbfc
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-15 08:03:31 +0300

    #150: New input and state for throwing grenade

--- Source/Blaster/BlasterTypes/CombatState.h
+++ Source/Blaster/BlasterTypes/CombatState.h
@@ -5,5 +5,6 @@ enum class ECombatState : uint8
 {
     ECS_Unoccupied UMETA(DisplayName = "Unoccupied"),
     ECS_Reloading UMETA(DisplayName = "Reloading"),
+    ECS_ThrowingGrenade UMETA(DisplayName = "Throwing grenade"),
     ECS_MAX UMETA(DisplayName = "DefaultMAX")
 };
