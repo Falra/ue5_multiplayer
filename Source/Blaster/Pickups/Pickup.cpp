3510a9136c4d2517f09b0a4ec448c3d6a10cba3a
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-16 16:56:23 +0300

    #158: Add basic pickup components

--- Source/Blaster/Pickups/Pickup.cpp
+++ Source/Blaster/Pickups/Pickup.cpp
@@ -3,17 +3,55 @@
 
 #include "Pickup.h"
 
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
 APickup::APickup()
 {
     PrimaryActorTick.bCanEverTick = true;
+    bReplicates = true;
+
+    RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+
+    OverlapSphere = CreateDefaultSubobject<USphereComponent>("OverlapSphere");
+    OverlapSphere->SetupAttachment(RootComponent);
+    OverlapSphere->SetSphereRadius(150.0f);
+    OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    OverlapSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+    OverlapSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+    PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickupMesh");
+    PickupMesh->SetupAttachment(OverlapSphere);
+    PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    
 }
 
 void APickup::BeginPlay()
 {
     Super::BeginPlay();
+    if (HasAuthority())
+    {
+        OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+    }
+}
+
+void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
 }
 
 void APickup::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+}
+
+void APickup::Destroyed()
+{
+    if (PickupSound)
+    {
+        UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
+    }
+        
+    Super::Destroyed();
 }
--- Source/Blaster/Pickups/Pickup.h
+++ Source/Blaster/Pickups/Pickup.h
@@ -12,7 +12,21 @@ class BLASTER_API APickup : public AActor
 public:
     APickup();
     virtual void Tick(float DeltaTime) override;
+    virtual void Destroyed() override;
 protected:
     virtual void BeginPlay() override;
+
+    UFUNCTION()
+    virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+private:
+    UPROPERTY(EditAnywhere)
+    class USphereComponent* OverlapSphere;
+
+    UPROPERTY(EditAnywhere)
+    UStaticMeshComponent* PickupMesh;
+    
+    UPROPERTY(EditAnywhere)
+    class USoundCue* PickupSound;
 public:
 };
