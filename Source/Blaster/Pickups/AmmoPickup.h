e318835d4096d2b7abbe181b95a69e1091a46cc1
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-17 08:17:51 +0300

    #159: Add blank derived ammo pickup class

---
+++ Source/Blaster/Pickups/AmmoPickup.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "AmmoPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AAmmoPickup : public APickup
+{
+    GENERATED_BODY()
+};
