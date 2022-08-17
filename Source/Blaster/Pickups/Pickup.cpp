422ea4c2d64786c8a8f930c5b61c8ceafaff4a87
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-17 09:19:39 +0300

    #159: Adjust mesh visual params

--- Source/Blaster/Pickups/Pickup.cpp
+++ Source/Blaster/Pickups/Pickup.cpp
@@ -50,7 +50,7 @@ void APickup::Tick(float DeltaTime)
     Super::Tick(DeltaTime);
     if (PickupMesh && BaseTurnRate != 0.0f)
     {
-        PickupMesh->AddLocalRotation(FRotator(0.0f, BaseTurnRate * DeltaTime, 0.0f));
+        PickupMesh->AddWorldRotation(FRotator(0.0f, BaseTurnRate * DeltaTime, 0.0f));
     }
 }
 
