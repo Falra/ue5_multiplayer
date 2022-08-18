bdf7f15d5a36c83672c5e12c02cb44e4b16f624e
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-18 19:49:56 +0300

    #163: Add blank class for speed pickup

---
+++ Source/Blaster/Pickups/SpeedPickup.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "SpeedPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ASpeedPickup : public APickup
+{
+    GENERATED_BODY()
+};
