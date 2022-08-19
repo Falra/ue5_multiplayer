1d6dedda181c9b1d4e8b0c54ed48c389c6c3ce02
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-19 08:35:58 +0300

    #164: Add blank class for jump pickup

---
+++ Source/Blaster/Pickups/JumpPickup.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "JumpPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AJumpPickup : public APickup
+{
+    GENERATED_BODY()
+};
