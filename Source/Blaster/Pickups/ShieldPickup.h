68592b9cd0766c360df3d39ee9e781e3398e96c6
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-20 09:28:45 +0300

    #167: Add blank class shield pickup

---
+++ Source/Blaster/Pickups/ShieldPickup.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "ShieldPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AShieldPickup : public APickup
+{
+    GENERATED_BODY()
+};
