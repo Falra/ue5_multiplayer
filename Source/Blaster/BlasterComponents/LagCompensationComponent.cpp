8a9757e030ae30ae986832df051fb3433c51ee32
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-08-27 09:55:58 +0300

    #187: Add blank class for lag compensation component

---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,19 @@
+// Blaster Game. All Rights Reserved
+
+
+#include "LagCompensationComponent.h"
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void ULagCompensationComponent::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
