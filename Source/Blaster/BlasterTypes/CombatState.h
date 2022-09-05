457fabadbe81dcde05db087d4c99d0b7f46174af
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-05 08:33:19 +0300

    #207: Add Swapping weapons state

--- Source/Blaster/BlasterTypes/CombatState.h
+++ Source/Blaster/BlasterTypes/CombatState.h
@@ -6,5 +6,6 @@ enum class ECombatState : uint8
     ECS_Unoccupied UMETA(DisplayName = "Unoccupied"),
     ECS_Reloading UMETA(DisplayName = "Reloading"),
     ECS_ThrowingGrenade UMETA(DisplayName = "Throwing grenade"),
+    ECS_SwappingWeapons UMETA(DisplayName = "Swapping weapons"),
     ECS_MAX UMETA(DisplayName = "DefaultMAX")
 };
