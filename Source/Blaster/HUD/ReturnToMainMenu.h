1c32589f84b05523b367f9a85fbff69f19cf5e21
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-05 17:02:31 +0300

    #211: Add blank class for return to menu button

---
+++ Source/Blaster/HUD/ReturnToMainMenu.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ReturnToMainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API UReturnToMainMenu : public UUserWidget
+{
+    GENERATED_BODY()
+};
