5fc79e3cb7ac59d719778031680eceab4a2ff0b1
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-07 08:46:33 +0300

    #216: Add set elimination text logic, re-parent WBP

--- Source/Blaster/HUD/ElimAnnouncement.cpp
+++ Source/Blaster/HUD/ElimAnnouncement.cpp
@@ -2,3 +2,13 @@
 
 
 #include "ElimAnnouncement.h"
+
+#include "Components/TextBlock.h"
+
+void UElimAnnouncement::SetElimAnnouncement(FString AttackerName, FString VictimName)
+{
+    if (!AnnouncementText) return;
+
+    FString ElimAnnouncementText = FString::Printf(TEXT("%s eliminated %s"), *AttackerName, *VictimName);
+    AnnouncementText->SetText(FText::FromString(ElimAnnouncementText));
+}
--- Source/Blaster/HUD/ElimAnnouncement.h
+++ Source/Blaster/HUD/ElimAnnouncement.h
@@ -13,4 +13,12 @@ UCLASS()
 class BLASTER_API UElimAnnouncement : public UUserWidget
 {
     GENERATED_BODY()
+public:
+    void SetElimAnnouncement(FString AttackerName, FString VictimName);
+    
+    UPROPERTY(meta = (BindWidget))
+    class UHorizontalBox* AnnouncementBox;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* AnnouncementText;
 };
