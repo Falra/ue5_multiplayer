a94f81131708a2d4c2002722f5a4e07e4761992f
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-07 08:40:55 +0300

    #216: Add elimination WBP and blank widget class

---
+++ Source/Blaster/HUD/ElimAnnouncement.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ElimAnnouncement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API UElimAnnouncement : public UUserWidget
+{
+    GENERATED_BODY()
+};
