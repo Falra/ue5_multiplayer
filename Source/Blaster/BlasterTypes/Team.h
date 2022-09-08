be14b898ec73f5610598284d63ef2370daad6111
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-08 08:10:39 +0300

    #222: Add team enum

---
+++ Source/Blaster/BlasterTypes/Team.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class ETeam : uint8
+{
+    ET_RedTeam UMETA(DisplayName = "Red Team"),
+    ET_BlueTeam UMETA(DisplayName = "Blue Team"),
+    ET_NoTeam UMETA(DisplayName = "No Team"),
+    
+    ET_MAX UMETA(DisplayName = "DefaultMAX")
+};
