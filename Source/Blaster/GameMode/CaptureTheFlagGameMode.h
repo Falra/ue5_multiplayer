c4c46426e2bd0c643c93ceb9aab8dcb6f34e4576
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-09 14:44:04 +0300

    #238: Add blank class for capture the flag game

---
+++ Source/Blaster/GameMode/CaptureTheFlagGameMode.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TeamGameMode.h"
+#include "CaptureTheFlagGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ACaptureTheFlagGameMode : public ATeamGameMode
+{
+    GENERATED_BODY()
+};
