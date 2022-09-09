de8d622b617ba536c4331861080d068d5cd42363
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-09 08:04:01 +0300

    #232: Add special weapon type for flag

---
+++ Source/Blaster/Weapon/Flag.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "Flag.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AFlag : public AWeapon
+{
+    GENERATED_BODY()
+};
