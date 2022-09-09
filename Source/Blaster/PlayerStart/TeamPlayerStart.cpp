9caf4d8a62055c8a88e428937e1dcbecad6bbf4f
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-09 14:21:17 +0300

    #237: Add blank class for team player start

---
+++ Source/Blaster/PlayerStart/TeamPlayerStart.cpp
@@ -0,0 +1,4 @@
+// Blaster Game. All Rights Reserved
+
+
+#include "TeamPlayerStart.h"
---
+++ Source/Blaster/PlayerStart/TeamPlayerStart.h
@@ -0,0 +1,16 @@
+// Blaster Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "TeamPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ATeamPlayerStart : public APlayerStart
+{
+    GENERATED_BODY()
+};
